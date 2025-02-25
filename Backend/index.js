a1abd0c4e424f8d01a5fa2d5da20f2f698cd503a
Your Name <lawrencehome21@gmail.com>
2025-02-25 18:47:14 +0300

    created an API to create books

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModels.js';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 
+
+app.post('/books', async (req,res) => {
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            return res.status(400).send({ message: 'Fill all the required fields'})
+        } 
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        }
+
+        const book = await Book.create(newBook)
+        return res.status(201).send(book);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message })
+    }
+})
 
 
 mongoose.connect(process.env.MONGODB_URL)
