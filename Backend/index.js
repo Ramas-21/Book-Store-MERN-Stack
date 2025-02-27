713e20a040bf7623a8d14a798eeaec68452f36b7
Your Name <lawrencehome21@gmail.com>
2025-02-27 15:02:46 +0300

    handled the cors policy

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import booksRoute from './routes/booksRoute.js';
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 //middleware for parsing request body
 app.use(express.json());
-app.use('/books', booksRoute)
+app.use("/books", booksRoute);
+//middleware for handling cors policy
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 
 mongoose
   .connect(process.env.MONGODB_URL)
